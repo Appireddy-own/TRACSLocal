6719c1e8086881c32a7ecbd70dcade67528c5880
Appireddy <appi.tallapu@outlook.com>
2021-12-19 23:03:04 +0530

    Homepage redirect

--- TRACSLocal/App_Start/RouteConfig.cs
+++ TRACSLocal/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace TRACSLocal
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.MapPageRoute("Backup", "{*anything}", "~/TRACSHome.aspx");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
