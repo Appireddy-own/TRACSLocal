52bedc6a4e7f84c181e42923ead98da309d35dc0
Appireddy <appi.tallapu@outlook.com>
2021-12-20 08:23:19 +0530

    routing changes

--- TRACSLocal/App_Start/RouteConfig.cs
+++ TRACSLocal/App_Start/RouteConfig.cs
@@ -12,11 +12,11 @@ namespace TRACSLocal
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapPageRoute("Backup", "{*anything}", "~/TRACSHome.aspx");
+           // routes.MapPageRoute("WebFormDefault", "", "~/TRACSHome.aspx");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "ABSForm", action = "CreateABSForm", id = UrlParameter.Optional }
+                defaults: new { controller = "ABSForm", action = "RedirectToAspx", id = UrlParameter.Optional }
             );
         }
     }
